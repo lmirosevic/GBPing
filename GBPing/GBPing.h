942286248b65c138000f88b1aae8e53bc8d1d328
Luka Mirosevic <luka@goonbee.com>
2012-11-05 11:27:43 +0100

    Initial Commit

---
+++ GBPing/GBPing.h
@@ -0,0 +1,13 @@
+//
+//  GBPing.h
+//  GBPing
+//
+//  Created by Luka Mirosevic on 05/11/2012.
+//  Copyright (c) 2012 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBPing : NSObject
+
+@end
---
+++ GBPing/GBPing.m
@@ -0,0 +1,13 @@
+//
+//  GBPing.m
+//  GBPing
+//
+//  Created by Luka Mirosevic on 05/11/2012.
+//  Copyright (c) 2012 Goonbee. All rights reserved.
+//
+
+#import "GBPing.h"
+
+@implementation GBPing
+
+@end
